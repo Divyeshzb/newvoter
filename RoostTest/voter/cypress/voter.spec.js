712d0f2dd6db5f0888584f7506855473309a05e1
Divyeshzb <60085094+Divyeshzb@users.noreply.github.com>
2023-04-12 17:57:15 +0530

    Roost Test Commit

--- RoostTest/voter/cypress/voter.spec.js
+++ RoostTest/voter/cypress/voter.spec.js
@@ -1,9 +1,26 @@
+describe('Result component', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
 
+  it('displays the correct heading', () => {
+    cy.get('.heading').should('contain', 'Developers preference for building K8S cluster, as of');
+  });
 
-describe('ROOST_SVC_URL', () => {
-  it('Visits the ROOST_SVC_URL and checks the title and background color', () => {
-    cy.visit(Cypress.env('ROOST_SVC_URL'))
-    cy.title().should('include', 'Roost')
-    cy.get('body').should('have.css', 'background-color', 'white')
-  })
-})
+  it('displays the correct logo', () => {
+    cy.get('.logo img').should('have.attr', 'src', 'kubernates.png');
+  });
+
+  it('displays the candidate cards', () => {
+    cy.get('.card').should('have.length.gte', 1);
+  });
+
+  it('displays the candidate name and percentage of votes', () => {
+    cy.get('.card').each(($card) => {
+      cy.wrap($card).within(() => {
+        cy.get('.cardContent').should('contain', 'candidate_name');
+        cy.get('.progressbar_front').should('have.css', 'width').and('contain', '%');
+      });
+    });
+  });
+});
